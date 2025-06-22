594576c7932427540e2bdd37fe387a4357ab12f7
AashrithaSura <surak638@gmail.com>
2025-06-22 20:59:34 +0530

    Fix case sensitivity: update empController reference and file name

--- routes/api/employeeRouter.js
+++ routes/api/employeeRouter.js
@@ -6,7 +6,7 @@ const {
     createEmployee,
     deleteEmployee,
     getEmployee
-} = require('../../controllers/empController');
+} = require('../../controllers/empcontroller');
 const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles')
 
